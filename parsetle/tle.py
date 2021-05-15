ef24bab95d12a3ab6919e50c379b0a0603c25a39
Craig Martek <craigmartek@gmail.com>
2021-05-15 15:47:17 -0400

    support 3le

--- parsetle/tle.py
+++ parsetle/tle.py
@@ -1,5 +1,5 @@
 from dataclasses import dataclass
-from typing import List
+from typing import List, Optional
 import datetime
 import enum
 import io
@@ -11,6 +11,7 @@ class Classification(enum.Enum):
 
 @dataclass
 class TLE:
+    name: Optional[str]
     satellite_catalog_num: str
     classification: Classification
     launch_year: str
@@ -34,39 +35,43 @@ class TLE:
 
     @staticmethod
     def fromlines(lines: List[str]) -> 'TLE':
-        if len(lines) != 2:
-            raise ValueError('2 lines expected')
-        if len(lines[0]) != 69 or len(lines[1]) != 69:
+        if len(lines) < 2 or len(lines) > 3:
+            raise ValueError('2 or 3 lines expected')
+
+        (l0, l1, l2) = lines if len(lines) == 3 else [None, *lines]
+
+        if len(l1) != 69 or len(l2) != 69:
             raise ValueError('invalid line length')
-        if lines[0][0] != '1' or lines[1][0] != '2':
+        if (l0 and l0[0] != '0') or l1[0] != '1' or l2[0] != '2':
             raise ValueError('unexpected line sequence')
-        if lines[0][2:7] != lines[1][2:7]:
+        if l1[2:7] != l1[2:7]:
             raise ValueError('catalog numbers do not match')
-        if not _checksum(lines[0][:-1], int(lines[0][-1])):
+        if not _checksum(l1[:-1], int(l1[-1])):
             raise ValueError('line 1 failed checksum')
-        if not _checksum(lines[1][:-1], int(lines[1][-1])):
+        if not _checksum(l2[:-1], int(l2[-1])):
             raise ValueError('line 2 failed checksum')
         return TLE(
-            str(int(lines[0][2:7])) if lines[0][2].isdigit() else lines[0][2:7].strip(),
-            Classification(lines[0][7]),
-            lines[0][9:11],
-            int(lines[0][11:14].strip() or 0),
-            lines[0][14:17].strip(),
-            _parse_epoch(lines[0][18:20], lines[0][20:32].strip()),
-            float(lines[0][33:43]),
-            float(f'{lines[0][44]}.{lines[0][45:50]}e{lines[0][50:52]}'),
-            float(f'{lines[0][53]}.{lines[0][54:59]}e{lines[0][59:61]}'),
-            int(lines[0][62]),
-            int(lines[0][64:68]),
-            float(lines[1][8:16]),
-            float(lines[1][17:25]),
-            float(f'.{lines[1][26:33]}'),
-            float(lines[1][34:42]),
-            float(lines[1][43:51]),
-            float(lines[1][52:62]),
-            int(lines[1][63:68]),
-            int(lines[0][-1]),
-            int(lines[1][-1])
+            l0[1:].strip() if l0 else None,
+            str(int(l1[2:7])) if l1[2].isdigit() else l1[2:7].strip(),
+            Classification(l1[7]),
+            l1[9:11],
+            int(l1[11:14].strip() or 0),
+            l1[14:17].strip(),
+            _parse_epoch(l1[18:20], l1[20:32].strip()),
+            float(l1[33:43]),
+            float(f'{l1[44]}.{l1[45:50]}e{l1[50:52]}'),
+            float(f'{l1[53]}.{l1[54:59]}e{l1[59:61]}'),
+            int(l1[62]),
+            int(l1[64:68]),
+            float(l2[8:16]),
+            float(l2[17:25]),
+            float(f'.{l2[26:33]}'),
+            float(l2[34:42]),
+            float(l2[43:51]),
+            float(l2[52:62]),
+            int(l2[63:68]),
+            int(l1[-1]),
+            int(l2[-1])
         )
 
 
@@ -93,11 +98,11 @@ class TLEFile:
     @staticmethod
     def fromfile(f: io.TextIOWrapper):
         tles = []
-        line1 = f.readline()
-        line2 = f.readline()
-        while line1 and line2:
-            tles.append(TLE.fromlines([line1.strip(), line2.strip()]))
-            line1 = f.readline()
-            line2 = f.readline()
+        current_lines = []
+        while line := f.readline().strip():
+            current_lines.append(line)
+            if line.startswith('2'):
+                tles.append(TLE.fromlines(current_lines))
+                current_lines = []
 
         return tles
