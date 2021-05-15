62da98c7faf68551551efe42277ad84d754a72dd
Craig Martek <craigmartek@gmail.com>
2021-05-14 20:52:30 -0400

    add setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='parsetle',
+    version='0.1',
+    description='Parses two-line element set files',
+    url='https://github.com/craigatron/parse-tle',
+    author='Craig Martek',
+    author_email='craigmartek@gmail.com',
+    license='MIT',
+    packages=['parsetle'],
+    zip_safe=False
+)
